37006e716b0947cf1be9158af49aa5d8edb81efa
Ujjwal Tak <24545193+codefoxut@users.noreply.github.com>
2022-07-18 09:32:19 +0530

    darkmode state handling added

---
+++ nuxt-projects/nuxt3-course-udemy/pages/dark-mode.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    const { isDarkMode } = useDarkMode();
+</script>
+
+<template>
+  <div :style="isDarkMode ? {background: 'black'} : null">
+    {{ isDarkMode }}
+    <DarkModeNav />
+    <DarkModeHeading />
+    <DarkModeCards />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Georgia;
+}
+</style>
