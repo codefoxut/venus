28ff0056b00c94d4c51a0a8fc2f9a26d05a0d3fb
Ujjwal Tak <24545193+codefoxut@users.noreply.github.com>
2023-06-27 12:19:13 +0530

    project 03 cartrader home page

---
+++ nuxt-projects/nuxt3-course-udemy/project03/components/Car/Hero.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- HOME HERO --->
+    <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://pictures.porsche.com/rtt/iris?COSY-EU-100-1711coMvsi60AAt5FwcmBEgA4qP8iBUDxPE3Cb9pNXABuN9dMGF4tl3U0%25z8rMHIspMtwZaiJQP0iO5Mi1PHcTCkqHGXoq1I9Xr6FObMP4wRuT0kuix7e2HsqQD9AzXCKuAIKyDUZv6Ot5FTmnEBc6XdGC')]">
+        <div class="z-10 absolute w-full h-full flex justify-center items-center">
+          <div class="text-center w-1/>2">
+            <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+            <CarSearchBar />
+          </div>
+        </div>
+        <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+      </div>
+      
+      <!-- HOME HERO --->
+</template>
