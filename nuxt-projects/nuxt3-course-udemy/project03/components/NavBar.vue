d64a7f79141b79db601456556a9e7479265e57c3
Ujjwal Tak <24545193+codefoxut@users.noreply.github.com>
2026-02-09 11:28:59 +0530

    nuxt3 upgrades

--- nuxt-projects/nuxt3-course-udemy/project03/components/NavBar.vue
+++ nuxt-projects/nuxt3-course-udemy/project03/components/NavBar.vue
@@ -1,5 +1,10 @@
 <script setup>
+const { user, logout } = useAuth()
 
+const handleLogout = () => {
+    logout()
+	navigateTo('/')
+}
 </script>
 
 <template>
@@ -10,7 +15,7 @@
 		<NuxtLink class="text-3xl font-mono" to="/">cartrader</NuxtLink>
 		<div v-if="user" class="flex">
 			<NuxtLink to="/profile/listings" class="mr-5">Profile</NuxtLink>
-			<p class="cursor-pointer" @click="logout">Logout</p>
+			<p class="cursor-pointer" @click="handleLogout">Logout</p>
 		</div>
 		<NuxtLink v-else to="/login">Login</NuxtLink>
 	</header>
--- nuxt-projects/nuxt3-course-udemy/project03/error.vue
+++ nuxt-projects/nuxt3-course-udemy/project03/error.vue
@@ -10,7 +10,7 @@
 
 <template>
 	<div class="flex h-screen justify-center items-center flex-col">
-		<h1 class="text-9xl">{{ error.statusCode }}</h1>
+		<h1 class="text-9xl">{{ error.status }}</h1>
 		<p class="mt-7 text-4xl">{{ error.message }}</p>
 		<button
 			@click="handleError"
